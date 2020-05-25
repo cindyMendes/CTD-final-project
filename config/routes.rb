9d033b07ae7019c917466807cca328e226635ffd
Cindy Mendes <cindymendes@MacBook-Air-de-Cindy.local>
2020-05-24 23:43:40 -0400

    Adding css

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   get 'welcome/dogs'
   get 'welcome/lists'
   get 'welcome/new'
-  get 'welcome/index'
-  get 'welcome/dogslists'
-  get 'welcome/new'
+  
   resources :lists
   resources :dogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
